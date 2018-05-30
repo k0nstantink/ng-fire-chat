a74d36e517e0d1971ca1bf111192b5f0485f8e71
k0nstantink <konstantin.konstantinovic@gmail.com>
2018-05-30 10:37:50 +0100

    firebase object required environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDQwmYG6nEExIRS-hWmg0c-Wk-ZHZ7gL4s',
+    authDomain: 'ng-demo-app-e57b4.firebaseapp.com',
+    databaseURL: 'https://ng-demo-app-e57b4.firebaseio.com',
+    projectId: 'ng-demo-app-e57b4',
+    storageBucket: 'ng-demo-app-e57b4.appspot.com',
+    messagingSenderId: '165064018329'
+  }
 };
